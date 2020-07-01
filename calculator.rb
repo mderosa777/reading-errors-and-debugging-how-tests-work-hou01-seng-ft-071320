2395f1669678bfed0f7a00898c62f33dea19a134
megan <meggsnbacon777@gmail.com>
2020-07-01 09:58:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = 0 
-second_number = 1 # Add your variables here
+first_number = 1
+second_number = 2 # Add your variables here
 sum = first_number+second_number
 sum
 difference = first_number - second_number
